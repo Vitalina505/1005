564d68f7f1dcc5b89e7c08dbd026b5fdc2c8f3dc
Bayda Vitalina <vitalinab58@gmail.com>
2025-06-01 20:24:42 +0300

    hw2

---
+++ homework1.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.pensize(10)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.color("red", "yellow")
+turtle.pendown()
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(150)
+
+turtle.done()
